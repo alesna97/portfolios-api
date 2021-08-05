fe3097ed5f602ee5c94029622c314637e875fc0c
anjas lesmana <anjas.lesmana@doterb.com>
2021-08-05 23:10:38 +0700

    fix destroy relation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :technologies
-  has_many :applications
+  has_many :applications, :dependent => :destroy
   mount_uploader :image, ImageUploader
 
   accepts_nested_attributes_for :applications, :allow_destroy => true
